041401cdfb61c218f95fb917e9d61671cec2bb74
Thiago P. Bueno <thiago.pbueno@gmail.com>
2021-03-21 17:26:15 -0300

    feat(solvers): add (unconstrained) iLQR class implementation
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- tests/test_solver_ilqr.py
+++ tests/test_solver_ilqr.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 
 from tfmpc.envs.cartpole.swingup import CartPoleSwingUp
-from tfmpc.solvers.ilqr import iLQR
+from tfmpc.solvers import ilqr
 
 from tests.conftest import sample_action, sample_state
 
@@ -15,22 +15,25 @@ def env():
 
 @pytest.fixture(scope="module")
 def solver(env):
-    return iLQR(env)
+    return ilqr.iLQR(env)
 
 
 def test_start(solver):
     x0 = solver.env.initial_state
     T = 100
-    states, actions, costs = solver.start(x0, T)
+    trajectory, total_cost = solver.start(x0, T)
 
-    assert states.shape == (T + 1, solver.env.state_size, 1)
-    assert actions.shape == (T, solver.env.action_size, 1)
-    assert costs.shape == (T + 1,)
+    assert trajectory.states.shape == (T + 1, solver.env.state_size, 1)
+    assert trajectory.actions.shape == (T, solver.env.action_size, 1)
+    assert trajectory.costs.shape == (T + 1,)
+
+    expected_total_cost = tf.reduce_sum(trajectory.costs)
+    assert tf.abs(total_cost - expected_total_cost) < 1e-4
 
 
 def test_derivatives(solver):
     T = 100
-    states, actions, costs = solver.start(solver.env.initial_state, T)
+    (states, actions, costs), _ = solver.start(solver.env.initial_state, T)
     models = solver.derivatives(states, actions)
 
     assert len(models) == 3
@@ -47,8 +50,117 @@ def test_derivatives(solver):
 
 
 def test_backward(solver):
-    pass
+    T = 100
+    (states, actions, costs), _ = solver.start(solver.env.initial_state, T)
+    f_model, l_model, l_final_model = solver.derivatives(states, actions)
+
+    n = solver.env.state_size
+    m = solver.env.action_size
+
+    controller, dV1, dV2, g_norm = solver.backward(
+        T, f_model, l_model, l_final_model)
+
+    assert controller.K.shape == (T, m, n)
+    assert controller.k.shape == (T, m, 1)
+
+    assert dV1.shape == ()
+    assert dV2.shape == ()
+    assert g_norm.shape == ()
 
 
 def test_forward(solver):
-    pass
+    T = 100
+    candidate, _ = solver.start(solver.env.initial_state, T)
+    f_model, l_model, l_final_model = solver.derivatives(
+        candidate.states, candidate.actions)
+
+    n = solver.env.state_size
+    m = solver.env.action_size
+
+    controller, dV1, dV2, g_norm = solver.backward(
+        T, f_model, l_model, l_final_model)
+
+    alpha = 1.0
+    trajectory, total_cost, residual = solver.forward(
+        candidate, controller, alpha)
+
+    assert trajectory.states.shape == (T + 1, n, 1)
+    assert trajectory.actions.shape == (T, m, 1)
+    assert trajectory.costs.shape == (T + 1,)
+    assert total_cost.shape == ()
+    assert residual.shape == ()
+
+
+def test_solve(solver):
+    n = solver.env.state_size
+    m = solver.env.action_size
+
+    T = 100
+    x0 = solver.env.initial_state
+    trajectory, num_iterations, converged = solver.solve(x0, T)
+
+    assert trajectory.states.shape == (T + 1, n, 1)
+    assert trajectory.actions.shape == (T, m, 1)
+    assert trajectory.costs.shape == (T + 1,)
+
+
+def test_hamiltonian(solver):
+    T = 100
+    (states, actions, costs), _ = solver.start(solver.env.initial_state, T)
+    f_model, l_model, l_final_model = solver.derivatives(states, actions)
+
+    n = solver.env.state_size
+    m = solver.env.action_size
+
+    V_model = ilqr.ValueModel(l_final_model.l_x, l_final_model.l_xx)
+
+    for t in range(T):
+        f_t_model = ilqr.DynamicsModel(*[m[t] for m in f_model])
+        l_t_model = ilqr.CostModel(*[m[t] for m in l_model])
+        Q_model = solver._hamiltonian(f_t_model, l_t_model, V_model)
+
+        assert Q_model.Q_x.shape == (n, 1)
+        assert Q_model.Q_u.shape == (m, 1)
+        assert Q_model.Q_xx.shape == (n, n)
+        assert Q_model.Q_uu.shape == (m, m)
+        assert Q_model.Q_ux.shape == (m, n)
+
+
+def test_controller(solver):
+    T = 100
+    (states, actions, costs), _ = solver.start(solver.env.initial_state, T)
+    f_model, l_model, l_final_model = solver.derivatives(states, actions)
+
+    n = solver.env.state_size
+    m = solver.env.action_size
+
+    V_model = ilqr.ValueModel(l_final_model.l_x, l_final_model.l_xx)
+    f_t_model = ilqr.DynamicsModel(*[m[-1] for m in f_model])
+    l_t_model = ilqr.CostModel(*[m[-1] for m in l_model])
+    Q_model = solver._hamiltonian(f_t_model, l_t_model, V_model)
+
+    K, k = solver._controller(Q_model.Q_uu, Q_model.Q_u, Q_model.Q_ux)
+
+    assert K.shape == (m, n)
+    assert k.shape == (m, 1)
+
+
+def test_value_update(solver):
+    T = 100
+    (states, actions, costs), _ = solver.start(solver.env.initial_state, T)
+    f_model, l_model, l_final_model = solver.derivatives(states, actions)
+
+    n = solver.env.state_size
+    m = solver.env.action_size
+
+    V_model = ilqr.ValueModel(l_final_model.l_x, l_final_model.l_xx)
+    f_t_model = ilqr.DynamicsModel(*[m[-1] for m in f_model])
+    l_t_model = ilqr.CostModel(*[m[-1] for m in l_model])
+    Q_model = solver._hamiltonian(f_t_model, l_t_model, V_model)
+
+    controller = solver._controller(Q_model.Q_uu, Q_model.Q_u, Q_model.Q_ux)
+
+    V_x, V_xx = solver._value_update(Q_model, controller)
+
+    assert V_x.shape == (n, 1)
+    assert V_xx.shape == (n, n)
--- tfmpc/solvers/ilqr.py
+++ tfmpc/solvers/ilqr.py
@@ -1,20 +1,15 @@
-import logging
 from collections import namedtuple
-import os
-import time
 
-import numpy as np
 import tensorflow as tf
-from tuneconfig import experiment
-
-from tfmpc.envs import diffenv
-from tfmpc.utils import optimization
-from tfmpc.utils import trajectory
-from tfmpc import loggers
 
 
 Trajectory = namedtuple("Trajectory", "states actions costs")
 Controller = namedtuple("Controller", "K k")
+DynamicsModel = namedtuple("DynamicsModel", "f f_x f_u")
+CostModel = namedtuple("CostModel", "l l_x l_u l_xx l_uu l_ux l_xu")
+FinalCostModel = namedtuple("FinalCostModel", "l l_x l_xx")
+ValueModel = namedtuple("ValueModel", "V_x V_xx")
+QModel = namedtuple("QModel", "Q_x Q_u Q_xx Q_uu Q_ux")
 
 
 class iLQR:
@@ -23,7 +18,8 @@ class iLQR:
 
     For details please see:
 
-    >> Synthesis and stabilization of complex behaviors through online trajectory optimization
+    >> Synthesis and stabilization of complex behaviors
+    >> through online trajectory optimization
     >> Tassa, Erez, and Todorov (2012)
     """
 
@@ -39,7 +35,7 @@ class iLQR:
             step_length_min=0.01,
             step_length_max=0.5,
             c1=0.0,
-            c2=0.5,
+            c2=1.0,
             residual_threshold=5e-3,
             g_norm_threshold=1e-9,
             **kwargs
@@ -73,10 +69,20 @@ class iLQR:
         self._current_total_cost = None
 
     def start(self, x0, T):
-        """Returns an initial feasible candidate trajectory."""
+        """Returns an initial feasible candidate trajectory.
+
+        Args:
+            x0: the initial state.
+            T: the horizon of the task.
+
+        Returns:
+            (Trajectory, total_cost): the integrated trajectory and its total cost.
+        """
         states = [x0]
         costs = []
         actions = []
+
+        total_cost = 0.0
 
         action = tf.zeros_like(self.env.action_space.sample())
 
@@ -91,19 +97,21 @@ class iLQR:
             state = tf.squeeze(next_state, axis=0)
             action = tf.squeeze(action, axis=0)
             cost = tf.squeeze(cost, axis=0)
+            total_cost += cost
 
             actions.append(action)
             states.append(state)
             costs.append(cost)
 
         final_cost = self.env.final_cost(state)
+        total_cost += final_cost
         costs.append(final_cost)
 
         states = tf.stack(states, axis=0)
         actions = tf.stack(actions, axis=0)
         costs = tf.stack(costs, axis=0)
 
-        return Trajectory(states, actions, costs)
+        return Trajectory(states, actions, costs), total_cost
 
     def derivatives(self, states, actions):
         """Returns the locally-approximated dynamics and cost model.
@@ -118,33 +126,114 @@ class iLQR:
         transition_model = self.env.get_linear_transition(states[:-1], actions)
         cost_model = self.env.get_quadratic_cost(states[:-1], actions)
         final_cost_model = self.env.get_quadratic_final_cost(states[-1])
-
         return transition_model, cost_model, final_cost_model
 
-    def backward(self, f_model, l_model, l_f_model):
+    def backward(self, T, f_model, l_model, l_final_model):
         """Implements the DDP backward pass.
 
         Args:
+            T: the horizon of the task.
             f_model: dynamics approximation model.
             l_model: running cost approximation model.
-            l_f_model: final cost approximation model.
+            l_final_model: final cost approximation model.
 
         Returns:
             (controller, dV1, dV2, g_norm): the controller, the corresponding
             expected value improvement terms, and the total gradient norm.
         """
+        K, k = [], []
+        dV1 = dV2 = 0.0
+        g_norm = 0.0
 
-    def forward(self, controller, alpha):
+        V_model = ValueModel(l_final_model.l_x, l_final_model.l_xx)
+
+        for t in range(T - 1, -1, -1):
+
+            # evaluation
+            f_t_model = DynamicsModel(*[m[t] for m in f_model])
+            l_t_model = CostModel(*[m[t] for m in l_model])
+            Q_model = self._hamiltonian(f_t_model, l_t_model, V_model)
+
+            # optimization
+            controller = self._controller(Q_model.Q_uu, Q_model.Q_u, Q_model.Q_ux)
+
+            K.insert(0, controller.K)
+            k.insert(0, controller.k)
+
+            # value update
+            V_model = self._value_update(Q_model, controller)
+
+            # expected improvement terms
+            dV1 += tf.matmul(controller.k, Q_model.Q_u, transpose_a=True)
+            dV2 += tf.matmul(
+                tf.matmul(controller.k, Q_model.Q_uu, transpose_a=True),
+                controller.k)
+
+            # gradient norm (stopping criteria)
+            g_norm += tf.reduce_sum(Q_model.Q_u ** 2)
+
+        K = tf.stack(K, axis=0)
+        k = tf.stack(k, axis=0)
+        dV1 = tf.squeeze(dV1)
+        dV2 = tf.squeeze(dV2)
+
+        return Controller(K, k), dV1, dV2, g_norm
+
+    def forward(self, candidate, controller, alpha):
         """Implements the DDP forward pass.
 
         Args:
+            candidate: the current candidate trajectory.
             controller: the (K, k) gain matrices of the recent controller.
             alpha: the line-search parameter.
 
         Returns:
             (trajectory, total_cost, residual): A new candidate trajectory and
-            its total cost and total residual.
+            its total cost and residual.
         """
+        x = candidate.states
+        u = candidate.actions
+
+        states = [x[0]]
+        actions = []
+        costs = []
+
+        total_cost = 0.0
+        residual = 0.0
+
+        x_t = x[0]
+
+        K, k = controller
+
+        for t, (K_t, k_t) in enumerate(zip(K, k)):
+            delta_x = x_t - x[t]
+            delta_u = alpha * k_t + tf.matmul(K_t, delta_x)
+
+            u_t = u[t] + delta_u
+
+            x_t = tf.expand_dims(x_t, axis=0)
+            u_t = tf.expand_dims(u_t, axis=0)
+
+            c_t = tf.squeeze(self.env.cost(x_t, u_t), axis=0)
+            x_t = tf.squeeze(self.env.transition(x_t, u_t), axis=0)
+            u_t = tf.squeeze(u_t, axis=0)
+
+            states.append(x_t)
+            actions.append(u_t)
+            costs.append(c_t)
+
+            total_cost += c_t
+            residual = max(residual, tf.reduce_max(tf.abs(delta_u)))
+
+        c_T = self.env.final_cost(x_t)
+        costs.append(c_T)
+        total_cost += c_T
+
+        states = tf.stack(states, axis=0)
+        actions = tf.stack(actions, axis=0)
+        costs = tf.stack(costs, axis=0)
+
+        return Trajectory(states, actions, costs), total_cost, residual
 
     def solve(self, x0, T, show_progress=True):
         """Solves the task by iteratively running backward-forward DDP passes.
@@ -158,9 +247,75 @@ class iLQR:
             show_progress: the boolean flag for progress monitoring.
 
         Returns:
-            (trajectory, num_iterations): the best candidate trajectory and
-            the number of iterations used.
+            (trajectory, num_iterations, converged): the best trajectory,
+            the number of iterations used, and the convergence flag.
         """
+
+        self._candidate, self._current_total_cost = self.start(x0, T)
+
+        converged = False
+
+        for n_iter in range(1, self.max_iterations + 1):
+
+            # local approximation
+            f_model, l_model, l_final_model = self.derivatives(
+                self._candidate.states,
+                self._candidate.actions
+            )
+
+            # backward
+            backward_done = False
+
+            while not backward_done:
+                try:
+                    controller, dV1, dV2, g_norm = self.backward(
+                        T, f_model, l_model, l_final_model
+                    )
+
+                    backward_done = True
+
+                except tf.errors.InvalidArgumentError as e: # Q_uu not positive-definite
+                    self._increase_regularization()
+
+                    if self._reg >= self.reg_max:
+                        converged = False
+                        return self._candidate, n_iter, converged
+
+            # forward
+            alpha = 1.0
+
+            for k in range(self.step_length_num):
+                alpha = self.step_length_factor ** k
+
+                trajectory, total_cost, residual = self.forward(
+                    self._candidate, controller, alpha
+                )
+
+                dV = self._expected_improvement(dV1, dV2, alpha)
+
+                if self._accept_criteria(total_cost, dV):
+                    self._candidate = trajectory
+                    self._current_total_cost = total_cost
+
+                    break
+
+            # regularization
+            if alpha > self.step_length_max:
+                self._decrease_regularization()
+
+            elif alpha <= self.step_length_min:
+                self._increase_regularization()
+
+                if self._reg >= self.reg_max:
+                    converged = False
+                    return self._candidate, n_iter, converged
+
+            # stopping criteria
+            if self._stopping_criteria(g_norm, residual):
+                converged = True
+                return self._candidate, n_iter, converged
+
+        return self._candidate, n_iter, converged
 
     def _hamiltonian(self, f_model, l_model, V_model):
         """Returns the Hamiltonian model.
@@ -171,8 +326,37 @@ class iLQR:
             V_model: the next state Value function model.
 
         Returns:
-            (Q_x, Q_u, Q_xx, Q_uu, Q_ux): the components of the Hamiltonin.
+            (QModel): the regularized model of the Hamiltonian.
         """
+        f_x = f_model.f_x
+        f_u = f_model.f_u
+        l_x = l_model.l_x
+        l_u = l_model.l_u
+        l_xx = l_model.l_xx
+        l_xu = l_model.l_xu
+        l_uu = l_model.l_uu
+        V_x = V_model.V_x
+        V_xx = V_model.V_xx
+
+        f_x_trans = tf.transpose(f_x)
+        f_u_trans = tf.transpose(f_u)
+
+        Q_x = l_x + tf.matmul(f_x_trans, V_x)
+        Q_u = l_u + tf.matmul(f_u_trans, V_x)
+
+        f_x_trans_V_xx = tf.matmul(f_x_trans, V_xx)
+        f_u_trans_V_xx = tf.matmul(f_u_trans, V_xx)
+        f_u_trans_V_xx_reg = tf.matmul(
+            f_u_trans, V_xx + self._reg * tf.eye(self.env.state_size))
+
+        Q_xx = l_xx + tf.matmul(f_x_trans_V_xx, f_x)
+        Q_uu = l_uu + tf.matmul(f_u_trans_V_xx, f_u)
+        Q_ux = tf.transpose(l_xu) + tf.matmul(f_u_trans_V_xx, f_x)
+
+        Q_uu_reg = l_uu + tf.matmul(f_u_trans_V_xx_reg, f_u)
+        Q_ux_reg = tf.transpose(l_xu) + tf.matmul(f_u_trans_V_xx_reg, f_x)
+
+        return QModel(Q_x, Q_u, Q_xx, Q_uu_reg, Q_ux)
 
     def _controller(self, Q_uu, Q_u, Q_ux):
         """Returns the controller for the Hamiltonian action-based components.
@@ -183,20 +367,71 @@ class iLQR:
             Q_ux: the action-state Hessian of the Q-function approximation.
 
         Returns:
-            (controller): the gains (K, k) of the controller.
+            (Controller): the gains (K, k) of the controller.
         """
+        R = tf.linalg.cholesky(Q_uu)
+        kK = -tf.linalg.cholesky_solve(R, tf.concat([Q_u, Q_ux], axis=1))
+        k = kK[:,:1]
+        K = kK[:,1:]
+        return Controller(K, k)
+
+    def _value_update(self, Q_model, controller):
+        """Returns the improved Value update for the current controller.
+
+        Args:
+            Q_model: the regularized model of the Hamiltonian.
+            controller: the (K, K) controller.
+
+        Returns:
+            (ValueModel): the (V_x, V_xx) optimal value approximation model.
+        """
+        Q_x = Q_model.Q_x
+        Q_u = Q_model.Q_u
+        Q_xx = Q_model.Q_xx
+        Q_uu = Q_model.Q_uu
+        Q_ux = Q_model.Q_ux
+        K, k = controller
+
+        K_trans = tf.transpose(K)
+        k_trans = tf.transpose(k)
+        K_trans_Q_uu = tf.matmul(K_trans, Q_uu)
+
+        V_x = tf.reshape(
+            (Q_x
+             + tf.matmul(Q_ux, k, transpose_a=True)
+             + tf.matmul(K_trans, Q_u)
+             + tf.matmul(K_trans_Q_uu, k)),
+            shape=[self.env.state_size, 1]
+        )
+
+        V_xx = tf.reshape(
+            (Q_xx
+             + tf.matmul(Q_ux, K, transpose_a=True)
+             + tf.matmul(K_trans, Q_ux)
+             + tf.matmul(K_trans_Q_uu, K)),
+            shape=[self.env.state_size, self.env.state_size]
+        )
+        V_xx = 1 / 2 * (V_xx + tf.transpose(V_xx))
+
+        return ValueModel(V_x, V_xx)
 
     def _increase_regularization(self):
         """Increases the current regularization parameter."""
+        self._reg = min(self.reg_max, self._reg * self.reg_factor)
 
     def _decrease_regularization(self):
         """Decreases the current regularization parameter."""
+        self._reg = max(self.reg_min, self._reg / self.reg_factor)
 
     def _expected_improvement(self, dV1, dV2, alpha):
         """Returns the expected value improvement."""
+        return -alpha * (dV1 + 0.5 * alpha * dV2)
 
     def _accept_criteria(self, total_cost, dV):
-        """Returns True if the candidate acceptance criteria is met. Otherwise, False."""
+        """Returns True iff the candidate acceptance criteria is met."""
+        z = (self._current_total_cost - total_cost) / dV
+        return self.c1 < z
 
     def _stopping_criteria(self, g_norm, residual):
-        """Returns True if stopping criteria is met. Otherwise, False."""
+        """Returns True iif stopping criteria is met."""
+        return (g_norm < self.g_norm_threshold) or (residual < self.residual_threshold)
